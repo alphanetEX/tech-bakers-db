c2cacb7052bbcac696a3eeec011bf7d3a5845174
AlphanetEX <24623630+alphanetEX@users.noreply.github.com>
2022-10-02 13:29:04 -0300

    minimal change in atribute of images, name was changed to -> filename

--- tech-bakers-dml.sql
+++ tech-bakers-dml.sql
@@ -2,6 +2,121 @@
 INSERT INTO  categories (name,createdAt,updatedAt) 
 VALUES ('En oferta','1000-01-01 00:00:00','1000-01-01 00:00:00');
 
-INSERT INTO  (name,createdAt,updatedAt) 
+INSERT INTO  categories (name,createdAt,updatedAt) 
 VALUES ('Destacados','1000-01-01 00:00:00','1000-01-01 00:00:00');
 
+--types--
+INSERT INTO types (name,createdAt,updatedAt) VALUES ('Tortas','1000-01-01 00:00:00','1000-01-01 00:00:00');
+INSERT INTO types (name,createdAt,updatedAt) VALUES ('Pastelería','1000-01-01 00:00:00','1000-01-01 00:00:00');
+INSERT INTO types (name,createdAt,updatedAt) VALUES ('Masas','1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+-- products --
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Cheesecake',2,5500,15,'New york Chessecake, con base de masa quebrada de limón y crema saborizada a Maracujá. Opcional, topping de Crumble o Salsa de fruta de la pasión.',20,2,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Lemon Pie',1,2500,9,'Base de Pâte sucrée aromatizada con vainas de Vainilla con el clásico Curd de Limón y el perfecto de un esponjoso Merengue Italiano',10,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Devils Cake',1,2900,5,'Bomba de Chocolate en tres texturas con un palet de Frutos Rojos y decoraciones de Figuras de Chocolate.',10,2,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Tarta de Mango',1,2500,5,'Genoise de Fruta de la Pasión acompañado de una crema sedosa de mango y decorado con Frutas Frescas y pétalos de flores comestibles',20,2,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Budin Glaseado',3,1500,15,'Es la versión más rica de Glaseado, con ciruelas, cascaritas de naranja, nueces, pasas de uva y cerezas. Elaborada de manera 100% artesanal.',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Budin Glaseado',3,1500,15,'Es la versión más rica de Glaseado, con ciruelas, cascaritas de naranja, nueces, pasas de uva y cerezas. Elaborada de manera 100% artesanal.',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Caja de Cookies',3,1200,5,'Preparación culinaria de pequeño tamaño, dulce, horneada y hecha normalmente a base de harina de trigo y con bolitas de chocolate',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Masitas',3,1100,5,'Son un rollos o pastas de masa de pan dulce, ahuecadas en el centro y rellenadas de únicos sabores.',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Pavlova',2,3500,5,'Torre de Pavlova compuesta de frambuesa y chocolate, y exquisitos toques de canela',20,2,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Rogel',1,3000,10,'Capas intercaladas de Dulce de Leche y masa Rogel, coronadas con merengue italiano',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Deditos Finos',3,800,5,'Ricos deditos crocrantes llenos de crema chantilly y cubiertos de un exquisito cocoa.',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Tarta tres chocolates',2,2800,5,'Mousse de chocolate negro, blanco y con leche, glaseado con un baño de chocolate negro. Hecho en nuestro obrador',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Flan Napolino',2,2500,5,'Es un postre de caramelo, en textura fundente de la leche condensada, queso crema y aroma de la vainilla, muy rica al paladar',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Tarta Red Velvet',1,3200,5,'Torta de terciopelo rojo que contiente chocolate con un color rojo oscuro o rojo brillante, preparado en capas cubierto con un glaseado de queso cremoso',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Volcán de chocolate',2,2500,5,'Es un postre muy atractivo tanto para la vista como el paladar, al ser picado deja verter chocolate derretido de su interior',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Milhoja',3,1200,5,'Milhoja crocante con relleno de arequipe sin azúcar  y crema de leche, decorada con una capa suave de arequipe endulzado con sucralosa',20,1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Pastel de cereal con leche',2,5000,50,'Esta receta de pastel hace alusión a aquellos recuerdos que muchos tenemos en la infancia y esperar pacientemente para saborear aquella última parte de la leche que sabía a tu cereal favorito.',20,2,'2022-09-22 04:11:06','2022-09-22 11:27:41');
+INSERT INTO products (name,typeId,price,discount,description,stock,categoryId,createdAt,updatedAt) 
+VALUES ('Masas finas surtidas',1,5000,15,'Caja llena de rica variedad de diferentes masas surtidas, para deleitarse con todas.',40,1,'2022-09-25 14:15:48','2022-09-25 17:43:08');
+
+
+--images--
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('Cheesecake.jpeg',1,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt)
+VALUES ('LemonPie.jpeg',2,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('DevilsCake.jpeg',3,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('TartaMango.jpeg',4,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('ParfaitChocolate.jpeg',5,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('BudinGlaseado.jpeg',6,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('cookies.jpeg',7,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('masitas.jpeg',8,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('queenPavlova.jpg',9,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('MiniRogel.JPG',10,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('Cheesecake.jpeg',11,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('DeditosFinos.JPG',12,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('tartatres.jpeg',13,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('flanNapo.jpg',14,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('TartaRed.jpg',15,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('milHoja.JPG',16,'1000-01-01 00:00:00','1000-01-01 00:00:00');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('pastelcereal.jpg',17,'2022-09-22 04:11:06','2022-09-22 04:11:06');
+
+INSERT INTO images (fileName,productId,createdAt,updatedAt) 
+VALUES ('masas.jpeg',18,'2022-09-25 14:15:48','2022-09-25 14:15:48');
